2387836c414836babff1c3a866754a671ceead87
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-10-16 03:59:09 +0300

    style fix

--- src/app/components/TweetsList/TweetsList.js
+++ src/app/components/TweetsList/TweetsList.js
@@ -1,26 +1,38 @@
 "use client";
-
+import { Dna } from "react-loader-spinner";
 import { fetchCards } from "@/service/axios";
 import styles from "./TweetsList.module.css";
 import { useState, useEffect } from "react";
 import { TweetCard } from "../TweetCard/TweetCard";
+import { filterOptions } from "@/service/constants";
 
 export const TweetsList = () => {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [error, setSetError] = useState(null);
   const [page, setPage] = useState(1);
   const [isNextPage, setIsNextPage] = useState(false);
+  // const [selectedOption, setSelectedOption] = useState(filterOptions[0]);
+  // const [filter, setFilter] = useState("all");
+
+  // const handleFilterChange = (option) => {
+  //   setSelectedOption(option);
+  //   setFilter(option.value);
+  // };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const data = await fetchCards(page);
         setUsers(data);
 
         const setUsersNext = await fetchCards(page + 1);
         setUsersNext.length !== 0 ? setIsNextPage(true) : setIsNextPage(false);
+        setLoading(false);
       } catch (error) {
         setSetError(error);
+        setLoading(false);
       }
     };
     fetchData();
@@ -41,7 +53,21 @@ export const TweetsList = () => {
 
   return (
     <section className={styles.section}>
+      {loading ? <Dna height={200} width={200} /> : ""}
       {error ? <h1>Something vent wrong, reload page</h1> : ""}
+      {/* <select
+        name="selectedFruit"
+        className={styles.select}
+        onChange={handleFilterChange}
+        value={selectedOption}
+      >
+        {filterOptions.map(({ value, label }) => (
+          <option value={value} key={value}>
+            {label}
+          </option>
+        ))}
+      </select> */}
+
       <ul className={styles.list}>
         {users.map(({ id, follow, avatar, followers, tweets, user }) => (
           <li key={id}>
---
+++ src/service/constants.js
@@ -0,0 +1,5 @@
+export const filterOptions = [
+  { value: "all", label: "SHOW ALL" },
+  { value: "follow", label: "FOLLOW" },
+  { value: "following", label: "FOLLOWING" },
+];
